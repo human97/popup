a3afc71c2755d5af02b3da25061c487a31fc048d
Alex <33989884+human97@users.noreply.github.com>
2020-05-28 16:32:26 +0400

    Update script.js
    fix script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,24 @@
 function popUpShow() {
-    if (document.cookie != "visit='pop-up был скрыт пользователем'") {
-        document.querySelector('.popup__wrap').classList.remove('hide');
+    if (document.cookie !== "visit='pop-up был скрыт пользователем'") {
+        document.querySelector('.popup__wrap').classList.remove('hide')
         document.onkeydown = function (e) {
-            if (e.keyCode == 27) popUpClose();
-        };
+            if (e.keyCode === 27) popUpClose()
+        }
         document.querySelectorAll('.popup__close').forEach(function (e) {
-            e.onclick = popUpClose;
-        });
-
+            e.onclick = popUpClose
+        })
     }
 }
 
-setTimeout(popUpShow, 3000);
+setTimeout(popUpShow, 3000) // время ожидание до появления pop-up
 
 function popUpClose() {
-    document.querySelector('.popup__wrap').classList.add('hide');
-    document.onkeydown = null;
-    let date = new Date(new Date().getTime() + 24 * 3600 * 1000);
-    document.cookie = "visit='pop-up был скрыт пользователем'; path=/; expires=" + date.toUTCString();
+    document.querySelector('.popup__wrap').classList.add('hide')
+    document.onkeydown = null
+    let date = new Date(new Date().getTime() + 24 * 3600 * 1000)
+    document.cookie = "visit='pop-up был скрыт пользователем'; path=/; expires=" + date.toUTCString()
 }
 
-document.querySelector('.popup').onclick = function (event) {
-    event.stopPropagation();
+document.querySelector('.popup').onclick = function (e) {
+    e.stopPropagation()
 }
