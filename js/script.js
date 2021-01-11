e07b340634849a3fe50b95a71900d21f44fe3582
Alex <33989884+human97@users.noreply.github.com>
2021-01-11 06:40:33 +0400

    Update script.js
    fix js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
-function popUpShow() {
-    if (document.cookie !== "visit='pop-up был скрыт пользователем'") {
-        document.querySelector('.popup__wrap').classList.remove('hide')
-        document.onkeydown = function (e) {
-            if (e.keyCode === 27) popUpClose()
-        }
-        document.querySelectorAll('.popup__close').forEach(function (e) {
-            e.onclick = popUpClose
-        })
-    }
-}
-
-setTimeout(popUpShow, 3000) // время ожидание до появления pop-up
-
-function popUpClose() {
+const popUpClose = () => {
     document.querySelector('.popup__wrap').classList.add('hide')
     document.onkeydown = null
     let date = new Date(new Date().getTime() + 24 * 3600 * 1000)
     document.cookie = "visit='pop-up был скрыт пользователем'; path=/; expires=" + date.toUTCString()
 }
 
-document.querySelector('.popup').onclick = function (e) {
-    e.stopPropagation()
+const popUpShow = () => {
+    if (document.cookie !== "visit='pop-up был скрыт пользователем'") {
+        document.querySelector('.popup__wrap').classList.remove('hide')
+        document.onkeydown = (e) => {
+            if (e.keyCode === 27) {
+                popUpClose()
+            }
+        }
+        document.querySelectorAll('.popup__close').forEach((e) => {
+            e.addEventListener('click', popUpClose)
+        })
+    }
 }
+
+setTimeout(popUpShow(), 3000)
+
+document.querySelector('.popup').addEventListener('click', (e) => {
+    e.stopPropagation()
+})
